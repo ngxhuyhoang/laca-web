9812f673ddbeb785a491459cd5ec9a771db6e86d
Huy Hoang Nguyen <hoangnh0099@gmail.com>
2021-03-22 23:45:19 +0700

    feat: update

---
+++ .nuxt/components/plugin.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+const components = {
+  AppDrawer: () => import('../..\\components\\app-drawer.vue' /* webpackChunkName: "components/app-drawer" */).then(c => c.default || c),
+  AppNavbar: () => import('../..\\components\\app-navbar.vue' /* webpackChunkName: "components/app-navbar" */).then(c => c.default || c)
+}
+
+for (const name in components) {
+  Vue.component(name, components[name])
+  Vue.component('Lazy' + name, components[name])
+}
