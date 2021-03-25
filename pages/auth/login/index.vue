10b5b346936cdd10b9289b520f3430592d913ad3
Huy Hoang Nguyen <hoanghn@bestay.vn>
2021-03-25 17:17:31 +0700

    feat: auth test

--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Login</h1>
+  <div>
+    <h1>Login</h1>
+    <v-btn @click="login()">Login</v-btn>
+    <v-btn @click="logout()">Logout</v-btn>
+  </div>
 </template>
+
+<script>
+import firebase from 'firebase/app'
+
+export default {
+  methods: {
+    async login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+
+      try {
+        const result = await firebase.auth().signInWithPopup(provider)
+
+        console.log(result)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    async logout() {
+      try {
+        const result = await firebase.auth().signOut()
+        console.log(result)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+  },
+}
+</script>
