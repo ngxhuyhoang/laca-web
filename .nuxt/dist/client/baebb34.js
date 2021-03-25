1130bf16f96e626ddd7acc37dd871e6eaaf16cd3
Huy Hoang Nguyen <hoanghn@bestay.vn>
2021-03-25 09:06:53 +0700

    feat: ignore build folder

--- .nuxt/dist/client/baebb34.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{439:function(e,n,r){"use strict";r.r(n);var o=r(417);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","8.3.1","app")}}]);
